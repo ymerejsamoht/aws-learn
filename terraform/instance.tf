a0433536c15d938329708465f18f931b5314c79d
Spacehat <ymerejsamoht@gmail.com>
2018-03-20 01:37:30 -0400

    testing an initial create

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,4 +1,14 @@
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
+  key_name = "${var.AWS_SSH_KEY_PAIR}"
+
+  provisioner "file" {
+    source = "/tmp/testfile.txt"
+    destination = "/tmp/testfile.txt"
+    connection {
+      user = "ec2-user"
+      private_key = "${file("${var.AWS_SSH_KEY_PEM}")}"
+    }
+  }
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,3 +1,5 @@
+variable "AWS_SSH_KEY_PAIR" {}
+variable "AWS_SSH_KEY_PEM" {}
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
@@ -9,3 +11,4 @@ variable "AMIS" {
     us-west-2 = "ami-7f43f307"
   }
 }
+
